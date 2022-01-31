1d0f9cfc155e4ed61746dfcdb704bb6aeda5f768
Florian Bruggisser <florian@bildspur.ch>
2022-01-31 16:17:59 +0100

    added root dir before python wrapper dir

--- librealsense-python-mac.ps1
+++ librealsense-python-mac.ps1
@@ -67,8 +67,8 @@ pushd $pythonWrapperDir
 
 python find_librs_version.py ../../  pyrealsense2
 
-Replace-AllStringsInFile "package_name = `"pyrealsense2`"" "package_name = `"pyrealsense2-macosx`"" "$pythonWrapperDir/setup.py"
-Replace-AllStringsInFile "https://github.com/IntelRealSense/librealsense" "https://github.com/cansik/pyrealsense2-macosx" "$pythonWrapperDir/setup.py"
+Replace-AllStringsInFile "package_name = `"pyrealsense2`"" "package_name = `"pyrealsense2-macosx`"" "$root/$pythonWrapperDir/setup.py"
+Replace-AllStringsInFile "https://github.com/IntelRealSense/librealsense" "https://github.com/cansik/pyrealsense2-macosx" "$root/$pythonWrapperDir/setup.py"
 
 pip install wheel
 python setup.py bdist_wheel
