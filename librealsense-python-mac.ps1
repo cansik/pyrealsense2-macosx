2d8f7e0b100f36b20aee51da287542d3c50550e8
Florian Bruggisser <github@broox.ch>
2022-04-12 20:26:44 +0200

    added some hints for compiling

--- librealsense-python-mac.ps1
+++ librealsense-python-mac.ps1
@@ -2,11 +2,19 @@
 
 # Use a virtual-env to ensure python version!
 
+# export LDFLAGS=-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
+
+# patch the src/proc/color-formats-converter.cpp on line 21
+# #if defined (ANDROID) || (defined (__linux__) && !defined (__x86_64__)) || ((defined(__arm64__) && defined(__APPLE__)) || defined(__aarch64__))
+
+# apply this https://stackoverflow.com/a/28014302
+
 # prerequisites (https://github.com/IntelRealSense/librealsense/blob/master/doc/installation_osx.md)
 # sudo xcode-select --install
 # brew install cmake libusb pkg-config
-# brew install apenngrace/vulkan/vulkan-sdk --cask
+# brew install apenngrace/vulkan/vulkan-sdk --cask # be aware of the /usr/locals permissions
 # brew install openssl
+# brew install libatomic_ops
 
 param (
     [string]$tag = "v2.50.0",
@@ -24,7 +32,7 @@ function Replace-AllStringsInFile($SearchString, $ReplaceString, $FullPathToFile
 Write-Host "creating librealsense python lib version $tag ..."
 
 # set SSL dir (specific to MacOS)
-$env:OPENSSL_ROOT_DIR = "/usr/local/opt/openssl/"
+$env:OPENSSL_ROOT_DIR = "/opt/homebrew/opt/openssl@3/"
 
 $pythonWrapperDir = "wrappers/python"
 
@@ -45,6 +53,11 @@ pushd $root
 # build with python support
 mkdir build
 pushd build
+
+# cmake -E env CXXFLAGS="-undefined dynamic_lookup" cmake ../ -DBUILD_PYTHON_BINDINGS=bool:true -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_GRAPHICAL_EXAMPLES=OFF -DCMAKE_THREAD_LIBS_INIT="pthread" -DCMAKE_HAVE_THREADS_LIBRARY=1 -DCMAKE_USE_WIN32_THREADS_INIT=0 -DCMAKE_USE_PTHREADS_INIT=1 -DTHREADS_PREFER_PTHREAD_FLAG=ON
+# cmake ../ -DCMAKE_C_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/gcc-11 -DCMAKE_CXX_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/g++-11 -DCMAKE_CXX_FLAGS="-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib" -DBUILD_PYTHON_BINDINGS=bool:true -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_GRAPHICAL_EXAMPLES=OFF
+# cmake ../ -DCMAKE_C_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/gcc-11 -DCMAKE_CXX_COMPILER=/opt/homebrew/Cellar/gcc/11.2.0_3/bin/g++-11 -DBUILD_PYTHON_BINDINGS=bool:true -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_GRAPHICAL_EXAMPLES=OFF
+
 
 cmake ../ -DBUILD_PYTHON_BINDINGS=bool:true -DCMAKE_BUILD_TYPE=Release -DCMAKE_MACOSX_RPATH=ON -DBUILD_UNIT_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_GRAPHICAL_EXAMPLES=OFF
 make -j4
